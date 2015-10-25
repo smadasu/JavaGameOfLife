e941c4a8dc9ac71bdf87308dba84cc8eda7595a3
Sreenath Madasu <Sreenath.Madasu@gmail.com>
2015-10-25 11:53:25 -0400

    running game of life

--- src/main/java/com/ms/Main.java
+++ src/main/java/com/ms/Main.java
@@ -2,17 +2,17 @@ package com.ms;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.IntStream;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Control;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.ColumnConstraints;
@@ -26,10 +26,10 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
-	private static final int NUMBER_OF_ROWS = 50;
-	private static final int NUMBER_OF_COLUMNS = 50;
-	
+
+	private static final int NUMBER_OF_ROWS = 20;
+	private static final int NUMBER_OF_COLUMNS = 20;
+
 	private AtomicBoolean run = new AtomicBoolean();
 
 
@@ -37,7 +37,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws InterruptedException {
 		run.set(false);
 		VBox vBox = new VBox();
-        vBox.setSpacing(20);
+		vBox.setSpacing(20);
 		// create a grid with some sample data.
 		GridPane grid = createGrid();
 
@@ -47,19 +47,22 @@ public class Main extends Application {
 		vBox.getChildren().addAll(grid, buttonBar);
 		layout.getChildren().add(vBox);
 		primaryStage.setScene(new Scene(layout, 400, 450));
-		new Thread(() -> {
-			while (run.get()) {
-				System.out.println("RUNNING");
-			}
-		}).start();
 		primaryStage.show();
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		executor.submit(() -> {
+			run.set(true);
+			//System.out.println("run = " + run.get());
+			while(run.get()) {
+				grid.getChildren().parallelStream().forEach(node -> applyBackground(node, grid));
+			}
+		});
 	}
 
 	private GridPane createGrid() {
 		GridPane grid = new GridPane();
 		grid.setStyle("-fx-background-color: palegreen; -fx-padding: 2; -fx-hgap: 2; -fx-vgap: 2;");
 		grid.setSnapToPixel(false);
-		
+
 		ColumnConstraints columnConstraints = new ColumnConstraints();
 		columnConstraints.setPercentWidth(100 / NUMBER_OF_COLUMNS);
 		columnConstraints.setHalignment(HPos.CENTER);
@@ -68,8 +71,8 @@ public class Main extends Application {
 		rowConstraints.setValignment(VPos.CENTER);
 		IntStream.range(0, NUMBER_OF_ROWS).forEach(row -> {
 			IntStream.range(0, NUMBER_OF_COLUMNS).forEach(column -> {
-				boolean onFlag = ((Math.round(Math.random() * 1000000) % 2) == 0);
-				Rectangle rectangle = new Rectangle(5, 5, onFlag ? Color.BLACK : Color.WHITE);
+				boolean onFlag = ((Math.round(Math.random() * 100) % 2) == 0);
+				Rectangle rectangle = new Rectangle(10, 10, onFlag ? Color.BLACK : Color.WHITE);
 				grid.add(rectangle, column, row);
 			});
 			grid.getColumnConstraints().add(columnConstraints);
@@ -77,68 +80,58 @@ public class Main extends Application {
 		});
 		return grid;
 	}
-    
-    /**
-     * Demonstrates the construction and usage of the {@link SegmentedButtonBar}
-     * @return
-     */
-    public HBox createSegmentedButtonBar() {
-        ToggleButton button1 = new ToggleButton("Start");
-        button1.setOnAction(e -> 
-	        Platform.runLater(() -> {
-	            run.set(true);
-	        })
-        );
-        ToggleButton button2 = new ToggleButton("Pause");
-        button2.setOnAction(e -> 
-	        Platform.runLater(() -> {
-	        	run.set(false);
-	        })
-	    );
-        
-        ToggleGroup group = new ToggleGroup();
-        group.getToggles().addAll(button1, button2);
-        group.selectToggle(button1);
-        
-        HBox displayBox = new HBox();
-        displayBox.setSpacing(20);
-        displayBox.setAlignment(Pos.CENTER);
-        
-        HBox  buttonBar = new HBox();
-        buttonBar.setSpacing(20);
-        buttonBar.getChildren().addAll(button1, button2);
-        
-        displayBox.getChildren().addAll(buttonBar);
-        
-        return displayBox;
-    }
+
+	/**
+	 * Demonstrates the construction and usage of the {@link SegmentedButtonBar}
+	 * @return
+	 */
+	public HBox createSegmentedButtonBar() {
+		ToggleButton button1 = new ToggleButton("Start");
+		button1.setOnAction(e -> run.set(true));
+		ToggleButton button2 = new ToggleButton("Pause");
+		button2.setOnAction(e -> run.set(false));
+
+		ToggleGroup group = new ToggleGroup();
+		group.getToggles().addAll(button1, button2);
+		group.selectToggle(button1);
+
+		HBox displayBox = new HBox();
+		displayBox.setSpacing(20);
+		displayBox.setAlignment(Pos.CENTER);
+
+		HBox  buttonBar = new HBox();
+		buttonBar.setSpacing(20);
+		buttonBar.getChildren().addAll(button1, button2);
+
+		displayBox.getChildren().addAll(buttonBar);
+
+		return displayBox;
+	}
 
 	private void applyBackground(Node node, GridPane grid) {
 		List<Node> children = grid.getChildren();
 		int ind = children.indexOf(node);
-		int currentRow = ind / 20;
-		int currentColumn = ind - (ind / 20) * 20;
-		int[] neighbors = { (currentRow - 1) * 20 + currentColumn - 1, (currentRow - 1) * 20 + currentColumn,
-				(currentRow - 1) * 20 + currentColumn + 1, currentRow * 20 + currentColumn - 1,
-				currentRow * 20 + currentColumn + 1, (currentRow + 1) * 20 + currentColumn - 1,
-				(currentRow + 1) * 20 + currentColumn, (currentRow + 1) * 20 + currentColumn + 1 };
+		int currentRow = ind / NUMBER_OF_ROWS;
+		int currentColumn = ind - (ind / NUMBER_OF_ROWS) * NUMBER_OF_COLUMNS;
+		int[] neighbors = { (currentRow - 1) * NUMBER_OF_ROWS + currentColumn - 1, (currentRow - 1) * NUMBER_OF_ROWS + currentColumn,
+			(currentRow - 1) * NUMBER_OF_ROWS + currentColumn + 1, currentRow * NUMBER_OF_ROWS + currentColumn - 1,
+			currentRow * NUMBER_OF_ROWS + currentColumn + 1, (currentRow + 1) * NUMBER_OF_ROWS + currentColumn - 1,
+			(currentRow + 1) * NUMBER_OF_ROWS + currentColumn, (currentRow + 1) * NUMBER_OF_ROWS + currentColumn + 1 };
 		long aliveNeighbors = Arrays.stream(neighbors).filter(index -> {
-			if (index < 0 || index > 399) {
+			if (index < 0 || index > (NUMBER_OF_ROWS * NUMBER_OF_COLUMNS - 1)) {
 				return false;
 			}
-			Control control1 = (Control) children.get(index);
-			return control1.getStyle().contains("black");
+			Rectangle rectangle = (Rectangle) children.get(index);
+			return rectangle.getFill().equals(Color.BLACK);
 		}).count();
-		Control control = (Control) node;
-		String currentStyle = control.getStyle();
-//		System.out.println("current style" + currentStyle);
-		if (currentStyle.contains("black") && (aliveNeighbors < 2 || aliveNeighbors > 3)) {
-			System.out.println("aliveNeighbors = " + aliveNeighbors);
-			System.out.println("dying");
-			control.setStyle("-fx-background-color: cornsilk; -fx-alignment: center;");
+		Rectangle currentRectangle = (Rectangle) node;
+		if (currentRectangle.getFill().equals(Color.BLACK) && (aliveNeighbors < 2 || aliveNeighbors > 3)) {
+			//System.out.println("aliveNeighbors = " + aliveNeighbors);
+			//System.out.println("dying");
+			currentRectangle.setFill(Color.WHITE);
 		} else if (aliveNeighbors == 3) {
-			System.out.println("bathuking");
-			control.setStyle("-fx-background-color: black; -fx-alignment: center;");
+			//System.out.println("bathuking");
+			currentRectangle.setFill(Color.BLACK);
 		}
 	}
 
